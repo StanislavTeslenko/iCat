f1e7eca5e6e1f1466216f6362edfacf20e119f0f
Stanislav Teslenko <stanislav.teslenko@gmail.com>
2020-05-01 13:45:00 +0300

    Initial Commit

---
+++ iCat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iCat
+//
+//  Created by Stanislav Teslenko on 01.05.2020.
+//  Copyright © 2020 Stanislav Teslenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
